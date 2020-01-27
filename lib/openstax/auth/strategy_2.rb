4ca248a9ac41c4ee2a8a154c5f56446a328cce5d
Steve Burkett <steve@burkettnet.com>
2020-01-27 11:12:09 -0800

    Get the test to work (WIP)

--- lib/openstax/auth/strategy_2.rb
+++ lib/openstax/auth/strategy_2.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'json/jwt'
 require 'active_support'
 require 'active_support/core_ext/object/blank'
 
@@ -17,51 +18,71 @@ module OpenStax
         return {} unless cookie.present?
 
         begin
+          encrypted = JSON::JWT.decode(
+            cookie,
+            encryption_private_key,
+            encryption_algorithm.to_s,
+            encryption_method.to_s
+          ).plain_text
+
           JSON::JWT.decode(
-            JSON::JWT.decode(
-              cookie,
-              encryption_private_key,
-              encryption_algorithm.to_s,
-              encryption_method.to_s
-            ).plain_text,
+            encrypted,
             signature_public_key,
             signature_algorithm
           )
-        rescue JSON::JWT::Exception, OpenSSL::Cipher::CipherError
+        rescue JSON::JWT::Exception, OpenSSL::Cipher::CipherError => ex
           nil
-        rescue ActiveSupport::MessageVerifier::InvalidSignature,
-          ActiveSupport::MessageEncryptor::InvalidMessage => e
-          {}
+        #rescue ActiveSupport::MessageVerifier::InvalidSignature,
+        #  ActiveSupport::MessageEncryptor::InvalidMessage => e
+        #  {}
         end
       end
 
       private
 
+      def cookie_name
+        cookie_name = OpenStax::Auth.configuration.strategy_2_cookie_name
+        raise 'Signature public key is not defined' if cookie_name.blank?
+
+        cookie_name
+      end
       def signature_public_key
-        signature_public_key = OpenStax::Auth.configuration.strategy_2_signature_public_key
-        signature_public_key = OpenSSL::PKey::RSA.new signature_public_key
+        raw_signature_public_key = OpenStax::Auth.configuration.strategy_2_signature_public_key
+        signature_public_key = OpenSSL::PKey::RSA.new raw_signature_public_key \
+          rescue raw_signature_public_key      #use literal key
         raise 'Signature public key is not defined' if signature_public_key.blank?
+
+        signature_public_key
       end
 
       def encryption_private_key
-        encryption_private_key = OpenStax::Auth.configuration.strategy_2_encryption_private_key
-        encryption_private_key = OpenSSL::PKey::RSA.new encryption_private_key
+        raw_encryption_private_key = OpenStax::Auth.configuration.strategy_2_encryption_private_key
+        encryption_private_key = OpenSSL::PKey::RSA.new raw_encryption_private_key \
+          rescue raw_encryption_private_key     #use literal key
         raise 'Encryption private key is not defined' if encryption_private_key.blank?
+
+        encryption_private_key
       end
 
       def signature_algorithm
         signature_algorithm = OpenStax::Auth.configuration.strategy_2_signature_algorithm
         raise 'Signature algorithm is not defined' if signature_algorithm.blank?
+
+        signature_algorithm
       end
 
       def encryption_algorithm
         encryption_algorithm = OpenStax::Auth.configuration.strategy_2_encryption_algorithm
         raise 'Encryption algorithm is not defined' if encryption_algorithm.blank?
+
+        encryption_algorithm
       end
 
       def encryption_method
         encryption_method = OpenStax::Auth.configuration.strategy_2_encryption_method
         raise 'Encryption method is not defined' if encryption_method.blank?
+
+        encryption_method
       end
     end
   end
--- spec/lib/openstax/auth/strategy_2_spec.rb
+++ spec/lib/openstax/auth/strategy_2_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'openstax/auth/strategy_2'
 
-RSpec.describe OpenStax::Auth::Strategy1 do
+RSpec.describe OpenStax::Auth::Strategy2 do
   SECRETS = {
     # these values copied from the Accounts secrets
     strategy_2_cookie_name: "oxa",
@@ -21,20 +21,20 @@ RSpec.describe OpenStax::Auth::Strategy1 do
   let(:mock_request) do
     OpenStruct.new(
       cookies: {
-        'oxa' => "ieyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..MBdVpgpCI_JWpcgq.ZFf9dxSRH-eNHo8AEFXBX" \
-                 "2j4QsFRxSGMmEfLG8TWjeI1Pbd7UW41PdR2H7ZwOnUG0zoy6BGF-tiNWXad0mFJvkfV1u3mLnZFXJu1D" \
-                 "Sn3MXysUSYjQkSkNRCicKEHo_Pio7ksoZ_CticcFWG8QRHJKHAa9vQsyBm09Fnpf3TPWXyOuPUlvM4hC" \
-                 "FVRTmSY3RWPf86ECpj7opVnR7G-Rgm3mB1bsUGVSH16zknaRbs2qXqR_tlpVDSBGD86hnX5cGppmqg6A" \
-                 "OQAncLLpQEVZxWJrJdehxO2ylgF2PK1RmkS6A1q0DGnzFODddMWG_abro7ijpFpd2eumqniDSBSPQKe7" \
-                 "EiMMoa36_YsV0m8L3kD84JgZ0XvQd4q-ZC7vL1RRYUgLCw2Lbd1l6pHNrxGF1rSB_p7KjDVkE1wpVc5t" \
-                 "1wI89TLQJLPYLyejcvVsdKecAijNyrzB396uRHOkpt1Hq5qiTiKDqAkLkCKAG6Lc0bea7hT349x2ZexG" \
-                 "QhHmeBCgS1LphTUef9MU0zD9eOQeF9yWl6DTMf2rW2uS_1XDKDNHXgGE-f1pLVN-vriZevQ1wX6eN_Op" \
-                 "TJs3_w7ugu2deU2Guj5Ggseg05vb7eMnYu8w8Cu7VGG5s3CXwLEzXbbMMf2MOXNVPr_sHZLit8WLz8dE" \
-                 "XKTu8rvnOgLzCRXQv4q9HEUjQaL3iloBq-use5pSqkS4EqAsOxDWLertWXDfe0fXL-43qzLrS_ahFtQn" \
-                 "7B4YhSvf9fEVaWcvx0st-7y7xIQP5FKVP9LWnTkhMlT2ho-7cS-91LftBn9X2oEEE4z6kDJ1H_CUUizD" \
-                 "DpyOrA_R2pdouos1tyhpKSqq7Th6W0Yaz_gleXoJYnX11wOBFBSefXyKgKlxUnK3DnMXaWq2g66sxqJz" \
-                 "Ju6A_fJhKQ_UuiTYOlnEj-akksFGxxqe6rdkS8WmKUqzuHYT6oSKX8xTLuvcXYnVVBhc3cNijDIkOz9C" \
-                 "HB62ZW733Yats4rbhquL2gRUxDYFsxc4RPcX4SfJ4v-.D0xgtTyz6N_gn9hVP9Dw5"
+        'oxa' => "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..MBdVpgpCI_JWpcgq.ZFf9dxSRH-eNHo8AEFXBX" \
+                 "2j4QsFRxSGMmEfLG8TWjeI1Pbd7UW41PdR2H7ZwOnUG0zoy6BGF-tiNWXad0mFJvkfV1u3mLnZFXJu1" \
+                 "DSn3MXysUSYjQkSkNRCicKEHo_Pio7ksoZ_CticcFWG8QRHJKHAa9vQsyBm09Fnpf3TPWXyOuPUlvM4" \
+                 "hCFVRTmSY3RWPf86ECpj7opVnR7G-Rgm3mB1bsUGVSH16zknaRbs2qXqR_tlpVDSBGD86hnX5cGppmq" \
+                 "g6AOQAncLLpQEVZxWJrJdehxO2ylgF2PK1RmkS6A1q0DGnzFODddMWG_abro7ijpFpd2eumqniDSBSP" \
+                 "QKe7EiMMoa36_YsV0m8L3kD84JgZ0XvQd4q-ZC7vL1RRYUgLCw2Lbd1l6pHNrxGF1rSB_p7KjDVkE1w" \
+                 "pVc5t1wI89TLQJLPYLyejcvVsdKecAijNyrzB396uRHOkpt1Hq5qiTiKDqAkLkCKAG6Lc0bea7hT349" \
+                 "x2ZexGQhHmeBCgS1LphTUef9MU0zD9eOQeF9yWl6DTMf2rW2uS_1XDKDNHXgGE-f1pLVN-vriZevQ1w" \
+                 "X6eN_OpTJs3_w7ugu2deU2Guj5Ggseg05vb7eMnYu8w8Cu7VGG5s3CXwLEzXbbMMf2MOXNVPr_sHZLi" \
+                 "t8WLz8dEXKTu8rvnOgLzCRXQv4q9HEUjQaL3iloBq-use5pSqkS4EqAsOxDWLertWXDfe0fXL-43qzL" \
+                 "rS_ahFtQn7B4YhSvf9fEVaWcvx0st-7y7xIQP5FKVP9LWnTkhMlT2ho-7cS-91LftBn9X2oEEE4z6kD" \
+                 "J1H_CUUizDDpyOrA_R2pdouos1tyhpKSqq7Th6W0Yaz_gleXoJYnX11wOBFBSefXyKgKlxUnK3DnMXa" \
+                 "Wq2g66sxqJzJu6A_fJhKQ_UuiTYOlnEj-akksFGxxqe6rdkS8WmKUqzuHYT6oSKX8xTLuvcXYnVVBhc" \
+                 "3cNijDIkOz9CHB62ZW733Yats4rbhquL2gRUxDYFsxc4RPcX4SfJ4v-.D0xgtTyz6N_gn9hVP9Dw5g"
       }
     )
   end
@@ -49,12 +49,6 @@ RSpec.describe OpenStax::Auth::Strategy1 do
                                                          "applications"=>[]
                                                        }
                                                      )
-  end
-
-  it 'returns whatever the cookie contains' do
-    encryptor = described_class.send(:encryptor)
-    mock_request.cookies['ox'] = encryptor.encrypt_and_sign(%w{ this is not a hash })
-    expect(described_class.decrypt(mock_request)).to eq(['this', 'is', 'not', 'a', 'hash'])
   end
 
   it 'returns empty on an invalid cookie' do
